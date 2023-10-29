a851281a0ad88fd5e44761643590b398d085c6a0
eangao <elmarcera@gmail.com>
2023-10-29 10:17:48 +0800

    Initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Stream from "./screens/Stream";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PDFSummary from "./screens/PDFSummary/PDFSummary";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stream" element={<Stream />} />
+          <Route path="/pdfsummary" element={<PDFSummary />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
